e28c81e73ed462df15a8ed1b7381fb3d3314eb23
arxsyy <marsyfernanda@gmail.com>
2025-04-09 22:29:58 +0700

    P6_Percobaan1B

--- src/Pertemuan6/SortingMain14.java
+++ src/Pertemuan6/SortingMain14.java
@@ -9,5 +9,13 @@ public class SortingMain14 {
         dataurut1.bubbleSort();
         System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC)");
         dataurut1.tampil();
+
+        int b[] = {30, 20, 2, 8, 14};
+        Sorting14 dataurut2 = new Sorting14(b, b.length);
+        System.out.println("Data awal 2");
+        dataurut2.tampil();
+        dataurut2.SelectionSort();
+        System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
+        dataurut2.tampil();
     }
 }
