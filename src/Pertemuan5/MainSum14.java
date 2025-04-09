302276b25ad900bf1ab352aece2bf2e85867c3cf
arxsyy <marsyfernanda@gmail.com>
2025-04-09 21:55:24 +0700

    P5_Percobaan3

---
+++ src/Pertemuan5/MainSum14.java
@@ -0,0 +1,24 @@
+package Pertemuan5;
+
+import java.util.Scanner;
+
+public class MainSum14 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Masukkan jumlah elemen : ");
+        int elemen = scanner.nextInt();
+        
+        Sum14 sm = new Sum14(elemen);
+        
+        for (int i = 0; i < elemen; i++) {
+            System.out.print("Masukkan keuntungan ke-" + (i + 1) + ": ");
+            sm.keuntungan[i] = scanner.nextDouble();
+        }
+        
+        System.out.println("Total keuntungan menggunakan Bruteforce : " + sm.totalBF());
+        System.out.println("Total keuntungan menggunakan Divide and Conquer : " + sm.totalDC(sm.keuntungan, 0, elemen - 1));
+        
+        scanner.close();
+    }
+}
