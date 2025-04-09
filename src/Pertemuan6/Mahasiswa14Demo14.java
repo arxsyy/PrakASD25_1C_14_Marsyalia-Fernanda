f63ff5717405a57efb73fd73dda0c40c9bc6a3ca
arxsyy <marsyfernanda@gmail.com>
2025-04-10 02:12:26 +0700

    P6_Percobaan2

---
+++ src/Pertemuan6/Mahasiswa14Demo14.java
@@ -0,0 +1,26 @@
+package Pertemuan6;
+
+public class Mahasiswa14Demo14 {
+    public static void main(String[] args) {
+    MahasiswaBerprestasi14 list = new MahasiswaBerprestasi14();
+    
+    Mahasiswa14 m1 = new Mahasiswa14("123", "Zidan", "2A", 3.2);
+    Mahasiswa14 m2 = new Mahasiswa14("124", "Ayu", "2A", 3.5);
+    Mahasiswa14 m3 = new Mahasiswa14("125", "Sofi", "2A", 3.1);
+    Mahasiswa14 m4 = new Mahasiswa14("126", "Sita", "2A", 3.9);
+    Mahasiswa14 m5 = new Mahasiswa14("127", "Miki", "2A", 3.7);
+    
+    list.tambah(m1);
+    list.tambah(m2);
+    list.tambah(m3);
+    list.tambah(m4);
+    list.tambah(m5);
+    
+    System.out.println("Data Mahasiswa sebelum sorting: ");
+    list.tampil();
+    
+    System.out.println("Data Mahasiswa setelah sorting berdasarkan IPK (DESC) : ");
+    list.bubbleSort();
+    list.tampil();
+}
+}
